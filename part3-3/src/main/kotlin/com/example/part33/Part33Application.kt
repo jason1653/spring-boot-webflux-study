0498012673ec42ac651bcf459940d7c12ae15ae6
jason <saga1654@kakao.com>
2023-10-30 11:27:33 +0900

    Router 샘플

---
+++ part3-3/src/main/kotlin/com/example/part33/Part33Application.kt
@@ -0,0 +1,11 @@
+package com.example.part33
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Part33Application
+
+fun main(args: Array<String>) {
+    runApplication<Part33Application>(*args)
+}
---
+++ part3-3/src/test/kotlin/com/example/part33/Part33ApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.part33
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Part33ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
